af5863b685a2288f9a2602105a06b66029c70b64
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-19 20:06:57 +0900

    앱 다운로드 버튼 애니메이션

--- js/pageChange.js
+++ js/pageChange.js
@@ -9,6 +9,10 @@ function pageDown(event, height) {
         // console.log(move)
         wrap.style.transform = `translate3d(0px, ${-move}px, 0px)`;
         
+        const aniTarget = wrap.children[nowPage].querySelector(".animation");
+        // console.log(aniTarget)
+        // if 
+
     }
 }
 
@@ -20,7 +24,7 @@ function pageUp(event, height) {
         // console.log(nowPage)
         // console.log("up")
         const move = nowPage * height
-        console.log(wrap)
+        // console.log(wrap)
         wrap.style.transform = `translate3d(0px, ${-move}px, 0px)`;
 
     }
@@ -31,6 +35,10 @@ function activeSection() {
     wrap.children[nowPage].classList.add("active");
 }
 
+
+//_________________________________________
+
+
 const wrap = document.querySelector("#section-wrap");
 const page = wrap.children;
 let nowPage = 0;
@@ -39,34 +47,55 @@ let check = true;
 
 // console.log(page.length)
 // console.log(wrap.children[2]);
+// console.dir(addEventListener)
+
+//_________________________________________
+// ANIMATION
+
+const app = document.querySelector("#download_btn");
+// console.log(app)
 
 
+//_________________________________________
+// eventlistener
 
-window.scrollTo(0, 0)
+// const reloadGotop = document.body.getBoundingClientRect();
+// window.scrollTo(reloadGotop.y, 0)
+
 window.addEventListener('wheel', function(event) {
     if(check === true){
-        check = false
+        check = false;
         event.preventDefault();
-        const height = window.innerHeight
-        console.log(event.deltaY)
+        const height = window.innerHeight;
+        // const aniTarget = wrap.children[nowPage].querySelector(".animation");
+        // console.log(aniTarget);
+        // console.log(event.deltaY);
         // console.dir(event)
     
         if(event.deltaY > 0) {
-            pageDown(event, height)
-            activeSection()
+            pageDown(event, height);
+            activeSection();
     
         }else if(event.deltaY < 0) {
-            pageUp(event, height)
-            activeSection()
+            pageUp(event, height);
+            activeSection();
         }
+        // animation
+        if (wrap.children[0].classList.contains("active")){
+            if (app.classList.contains("show")){
+                app.classList.add("hidden");
+            }
+            app.classList.remove("show");
+        }else{
+            app.classList.remove("hidden");
+            app.classList.add("show");
+        }
+
+
+
+
         setTimeout(function() {
             check = true;
         }, 500)
     }
 },{passive: false});
-
-// console.dir(addEventListener)
-
-
-
-
